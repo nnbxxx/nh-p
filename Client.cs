01e2651fb542f6a2582e9b61edf80627c649c052
nnbxxx <88110851+nnbxxx@users.noreply.github.com>
2022-10-30 19:42:50 +0700

    update client

--- Client.cs
+++ Client.cs
@@ -1,4 +1,4 @@
-class Client
+﻿class Client
 {
 
     private string name;
@@ -31,6 +31,11 @@ class Client
     private int fico;
     public int Fico { get { return fico; } set { fico = value; } }
 
+    // thêm thuộc tính ngày đăng ký thẻ
+
+    private DateTime registerdate;
+    public DateTime RegisterDate { get; set; }
+
     // public string[] ListCard =
     //         {
     //             "Napas Success", "Napas Success Plus"
@@ -41,7 +46,9 @@ class Client
     public SortedList<string, Loans> listLoans = new SortedList<string, Loans>();
     public Client()
     { }
-    public Client(string name, string address, DateTime dateofbirth, string id, string position, string nationality, string workplace, bool sex, string phonenumber, DateTime issueddate, decimal income)
+    public Client(string name, string address, DateTime dateofbirth, 
+        string id, string position, string nationality, string workplace, 
+        bool sex, string phonenumber, DateTime issueddate, decimal income)
     {
         this.Name = name;
         this.Address = address;
@@ -56,6 +63,27 @@ class Client
         this.PhoneNumber = phonenumber;
         this.IssuedDate = issueddate;
         this.Income = income;
+
+        // vì cái fico dựa vào thu nhập nên ta có thể tính luôn ở lúc khởi tạo
+        int tmp = 0;
+        if (Income < 5000000) 
+            tmp = 1;
+        else
+            if (Income >= 5000000 && Income < 10000000)
+            tmp = 2;
+        else
+            if (Income >= 10000000 && Income < 20000000)
+            tmp = 4;
+        else
+            if (Income >= 20000000 && Income < 50000000)
+            tmp = 7;
+        else
+            if (Income >= 50000000 )
+            tmp = 10;
+
+
+        this.Fico = tmp;
+
         Console.WriteLine("Tao tai khoan thanh cong!");
 
     }
@@ -128,4 +156,126 @@ class Client
             Console.WriteLine("Tao the thanh cong!");
         }
     }
+
+    //methods
+    public void CreateLoans(string TypeCard)
+    {
+
+    }
+
+    public void CreatePaymentAccount(string TypeCard)
+    {
+
+    }
+
+    public void CreateSavingAccount(string TypeCard)
+    {
+
+    }
+    public bool AgeCheck()
+    {
+        #region Hàm AgeCheck
+        /*
+             Trả về kiểu giá trị bool 
+             True: đủ 18 tuổi
+             False: ko đủ 18 tuổi
+         */
+        #endregion
+        DateTime A = this.DateOfBirth;
+        DateTime B = DateTime.Now;
+        TimeSpan Elapsed = B.Subtract(A);// tính khoảng cách giữa thời điểm B và A
+        double daysAgo = Elapsed.TotalDays;
+        if (daysAgo / 365 >= 18)
+        {
+            //Console.WriteLine("đủ tuổi");
+            return true;
+        }
+        else
+        {
+            return false;
+        } 
+            
+    }
+
+    public bool IDCheck(DateTime ExpirationDate)
+    {
+        #region Hàm IDCheck
+        /*
+             Trả về kiểu giá trị bool 
+             True: còn hạn
+             False: hết hạn
+         */
+        #endregion
+
+        if (ExpirationDate < DateTime.Now)
+        {
+            return true;
+        }    
+        else
+        {
+            return false;
+        }
+    }
+    public void SignUp(string name, string password)
+    {
+        this.NameUser = name;
+        this.Password = password;
+    }
+    public bool SignIn(string name, string password)
+    {
+        #region hàm SignIn
+        /*
+         Trả về kiểu dữ liệu bool 
+        True đăng nhập thành công
+        False đăng nhập thất bại
+         */ 
+        #endregion
+        return (this.NameUser == name) && (this.Password == password);
+    }
+
+    public bool ChangePassword(string OldPassword,string NewPassWord)
+    {
+        if(this.Password == OldPassword)
+        {
+            this.Password = NewPassWord;
+            return true;
+        }    
+        else
+        {
+            return false;
+        }    
+    }
+
+    public void ForgotPassword()
+    {
+        // chưa hiểu lắm
+    }
+
+    public void UpdateFico()
+    {
+        // ko nên để ở đây mà nên để trong từng loại thẻ có hàm update;
+    }    
+
+    public string ClassificationCustomer()
+    {
+        string[] TypeCustumer =
+        {
+            "Cooper","Silver","Gold","Platinum","Diamond"
+        };
+        int time = DateTime.Now.Year - this.RegisterDate.Year;
+        if (Fico > 20 && time > 1) return TypeCustumer[0];
+        if (Fico > 50 && time > 3) return TypeCustumer[1];
+        if (Fico > 80 && time > 4) return TypeCustumer[2];
+        if (Fico > 100 && time > 5) return TypeCustumer[3];
+        if (Fico > 2000 && time > 8) return TypeCustumer[4];
+        return "";
+
+    }
+
+    public void ExportInformation()
+    {
+        // xuất thông tin 
+    }
+
+
 }
--- Program.cs
+++ Program.cs
@@ -11,11 +11,15 @@ namespace Bank
                 "Napas Success", "Napas Success Plus"
             };
 
-            Client A = new Client("Duy", "HCM", new DateTime(day: 1, month: 1, year: 1), "21110397", "Sinh vien", "Viet Nam", "HCMUTE", true, "0111", new DateTime(day: 1, month: 1, year: 2018), 1);
+            Client A = new Client("Duy", "HCM", new DateTime(day: 5, month: 9, year: 2003), 
+                "21110397", "Sinh vien", "Viet Nam", "HCMUTE", true, "0111"
+                , new DateTime(day: 1, month: 1, year: 2018), 1);
+            
             A.CreateDebitCard(ListCard[0]);
             A.CreateDebitCard(ListCard[1]);
             A.listDebit[ListCard[0]].RutTien(20);
             A.listDebit[ListCard[1]].Ktratraikhoan();
+            A.AgeCheck();
 
 
         }
